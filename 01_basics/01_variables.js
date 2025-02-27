2383ed6b690572da4d480b19bb79625bd88103c1
sandeepbehra <sandeepbehra091998@gmail.com>
2025-02-27 19:06:09 +0530

    operations

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,5 +1,12 @@
 let Id = "14252"
 id2 = 99900
 
-console.log(typeof Id);
-console.log(typeof id2); 
+// console.log(typeof Id);
+// console.log(typeof id2); 
+// console.log(+"sss")
+// let id = ""
+// let newId = Boolean(id)
+// console.log(typeof newId)
+// console.log(newId)
+console.log(null>=0)
+// console.log(undefined>=0)
